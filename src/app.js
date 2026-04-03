95bf1e04c28651eedc68cce952ec1be445032894
Priya Pallavi <pallagmlaptop1185@gmail.com>
2026-04-03 11:25:15 +0530

    Day 1: Basic server setup and setting up mongoDB Atlas And DB

---
+++ src/app.js
@@ -0,0 +1,12 @@
+//This app.js file have mainly two main work
+//1.Initiate the server means to create a instance of a serve.
+//2.To use middleware and routes(the api we create).
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+
+module.exports = app;
