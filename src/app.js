e8b03f86e479235971cbf423cdb865b68541ddb3
Priya9304 <pallavipriya9304@gmail.com>
2026-04-14 03:03:05 +0530

    Day5 Created GetMe API, Auth Middleware & Controller

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@
 //1.Initiate the server means to create a instance of a serve.
 //2.To use middleware and routes(the api we create).
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 const authRouter = require("./routes/auth.routes");
 
 //api/auth is prefix we need to use before using the authRoutes
 app.use("/api/auth",authRouter);
-console.log("day4 Success");
 module.exports = app;
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const authController = require("../controller/auth.controller")
+const authController = require("../controller/auth.controller");
+const authMiddleware = require("../middleware/auth.middleware");
 
 const authRouter = express.Router();
 /**
@@ -7,13 +8,29 @@ const authRouter = express.Router();
  * @description Register a new user
  * @access public
  */
-authRouter.post("/register",authController.registerUserController);
+authRouter.get("/register",authController.registerUserController);
 
 /**
  * @route "./api/auth/login"
  * @description Login user with email amd password
  * @access public
  */
-
 authRouter.post("/login",authController.LoginUserController);
+
+/**
+ * @route "./api/auth/logoutr"
+ * @description clear token from cookie
+ * @access public
+ */
+
+authRouter.get("/logout",authController.logoutUserController);
+
+/**
+ * @route "./api/auth/get-me"
+ * @description get the current logged in user details
+ * @access public
+ */
+//If any user is requestion to this api,the details of that user is sended as a response from the database
+authRouter.get("/get-me",authMiddleware.authUser,authController.getMeController);
+
 module.exports = authRouter;
