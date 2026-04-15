7f063a773899412b775fdd4e5503ee23f808b6d0
Priya9304 <pallavipriya9304@gmail.com>
2026-04-16 01:01:23 +0530

    Day7 created login and reguster UI and link it with useNavigate hook and Link from react-router

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,21 +1,31 @@
 import React from 'react'
+import "../auth.form.scss"
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+    }
+
   return (
     <main>
         <div className="form-container">
             <h1>Login</h1>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="input-group">
-                    <label htmlFor="email"></label>
+                    <label htmlFor="email">Email</label>
                     <input type="email" id='email' name='email' placeholder='Enter Email' />
                 </div>
                 <div className="input-group">
-                    <label htmlFor="password"></label>
+                    <label htmlFor="password">Password</label>
                     <input type="password" id='password' name='password' placeholder='Enter Password' />
                 </div>
+                <button className="button primary-button">Login</button>
             </form>
+            <p>Don't have an account? <Link to={"/register"}>Register</Link></p>
+
         </div>
     </main>
   )
--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { useNavigate,Link} from 'react-router-dom'
 
 const Register = () => {
+    const naviagte = useNavigate() //for navigating from register page to login page
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+    }
   return (
-    <div>Register</div>
+    <main>
+        <div className="form-container">
+            <h1>Register</h1>
+
+            <form onSubmit={handleSubmit}>
+                <div className="input-group">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id='username' name='username' placeholder='Enter Username' />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id='email' name='email' placeholder='Enter Email' />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id='password' name='password' placeholder='Enter Password' />
+                </div>
+                <button className="button primary-button">Register</button>
+            </form>
+            <p>Already have an account? <Link to={"/login"}>Login</Link></p>
+
+        </div>
+    </main>
   )
 }
 
